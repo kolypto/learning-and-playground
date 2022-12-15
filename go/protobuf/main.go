f34aa8d9b7572bcd02112bb516735e199fcb24a5
Mark Vartanyan <kolypto@gmail.com>
2022-12-15 16:06:38 +0300

    protobuf example

--- go/protobuf/main.go
+++ go/protobuf/main.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+// $ sudo apt install protobuf-compiler  protobuf-compiler-grpc
+// $ go get google.golang.org/protobuf
+// $ go get google.golang.org/protobuf/cmd/protoc-gen-go@latest
 
+//go:generate protoc --go_out=protoc/go --python_out=protoc/python --experimental_allow_proto3_optional proto/example.proto
+
+import (
+	"fmt"
+	"goplay/protobuf/protoc/go/goplay/protobuf/goplaypb"
+	"log"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	if err := playProtobuf(); err != nil {
+		log.Fatalf("playProtobuf() failed: %+v", err)
+	}
+}
+
+
+
+func playProtobuf() error {
+	// Create
+	user := goplaypb.UserInfo{
+		Login: "kolypto",
+		Email: "kolypto@gmail.com",
+		Age: proto.Uint32(35),	 
+	}
+
+	// Marshal
+	out, err := proto.Marshal(&user)
+	if err != nil {
+		return err 
+	}
+
+	fmt.Printf("Marshal() user: %q\n", out)
+	
+
+	// Unmarshal
+	if err := proto.Unmarshal(out, &user); err != nil {
+		return err 
+	}
+	fmt.Printf("Unmarshal(): %v\n", user)
+
+	
+	// Marshal JSON
+	out, err = protojson.Marshal(&user)
+	if err != nil {
+		return err 
+	}
+	fmt.Printf("Marshal() JSON: %s\n", out)
+
+	// Marshal Text
+	out, err = prototext.Marshal(&user)
+	if err != nil {
+		return err 
+	}
+	fmt.Printf("Marshal() text: %s\n", out)
+
+	return nil
 }
