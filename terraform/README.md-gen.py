4a8db449cae50427d962980aec1ad43d79fe4bc4
Mark Vartanyan <kolypto@gmail.com>
2023-03-04 13:02:41 +0300

    terraform: README.md

---
+++ terraform/README.md-gen.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+import pathlib
+
+def main(files: list[str]):
+        prev_folder = None
+        for file_path in files:
+            file = pathlib.Path(file_path)
+
+            # Folder header
+            this_folder = str(file.parent)
+            if this_folder != prev_folder:
+                print(f'\n\n\n\n# {this_folder}')
+                prev_folder = this_folder
+
+            # File contents
+            contents = file.read_text()
+            # Markdowns: as is
+            if file.suffix == '.md':
+                print(contents)
+            else:
+                md_highlight = {
+                    '.tf': 'terraform',
+                    '.tfvars': 'terraform',
+                }[file.suffix]
+
+
+                # File header
+                print(f'\n\n# {file}\n')
+                print(f'```{md_highlight}\n{contents}\n```\n')
+
+
+
+
+files = sys.argv[1:]
+main(files)
