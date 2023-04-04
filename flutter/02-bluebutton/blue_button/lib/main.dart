d5935d6c7d49defceaa2b864335859f96389717a
Mark Vartanyan <kolypto@gmail.com>
2023-04-05 01:14:50 +0300

    blue button app

--- flutter/02-bluebutton/blue_button/lib/main.dart
+++ flutter/02-bluebutton/blue_button/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
-        ),
+            child: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                print("pressed");
+                sendRequest();
+              },
+              child: Text("Голубая Кнопка", textScaleFactor: 3)),
+        )),
       ),
     );
   }
+}
+
+sendRequest() async {
+  sendTelegramBotMessage('691814383', ':)'); // Me
+}
+
+sendTelegramBotMessage(String chatId, String text) async {
+  Map data = {
+    'chat_id': chatId,
+    'text': text,
+  };
+  const botKey = 'bot1842331905:AA....';
+  var url = Uri.parse('https://api.telegram.org/$botKey/sendMessage');
+  http.post(url, body: data).then((response) {
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
+  });
 }
